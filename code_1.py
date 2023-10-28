a8b0a4a8a4ecd22cbdfcb36a9a0c75f08189ee4c
vyrguskinys <144053476+vyrguskinys@users.noreply.github.com>
2023-10-28 10:45:49 +0200

    Update code_1.py

--- code_1.py
+++ code_1.py
@@ -1,26 +1,27 @@
-def generate_random_data():
-    data = [random.randint(1, 100) for _ in range(10)]
-
 if __name__ == "__main__":
 
+    for item in data:
     return data
+
+import random
+def generate_random_data():
     data = generate_random_data()
 
-    for item in data:
     main()
+
         print(f"Random Number: {item}")
 def main():
-import random
+    data = [random.randint(1, 100) for _ in range(10)]def main():
 
+    data = generate_random_data()
+    return data
+
+    main()
 import random
+    for item in data:
+
+        print(f"Random Number: {item}")
+def generate_random_data():
 
     data = [random.randint(1, 100) for _ in range(10)]
-    for item in data:
-    main()
 if __name__ == "__main__":
-def main():
-    data = generate_random_data()
-def generate_random_data():
-        print(f"Random Number: {item}")
-    return data
-
