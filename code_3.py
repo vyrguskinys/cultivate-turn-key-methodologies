673f02d7eae474d048ad56bd54fed6d6ff0c1755
vyrguskinys <144053476+vyrguskinys@users.noreply.github.com>
2023-11-02 15:29:53 +0100

    Update code_3.py

--- code_3.py
+++ code_3.py
@@ -1,68 +1,26 @@
-def main():
-    return data
-    for item in data:
-    data = [random.randint(1, 100) for _ in range(10)]
-def generate_random_data():
-        print(f"Random Number: {item}")
-
-    data = generate_random_data()
-
-if __name__ == "__main__":
-    main()
-import random
-
-    for item in data:
-
-
-    data = generate_random_data()
-def generate_random_data():
-        print(f"Random Number: {item}")
-if __name__ == "__main__":
-def main():
-    data = [random.randint(1, 100) for _ in range(10)]
-    main()
-
-import random
-    return data
-    return data;
-    for (let i = 0; i < 10; i++) {
-    const data = [];
-    const data = generateRandomData();
-        console.log("Random Number: " + item);
-function main() {
-        data.push(Math.floor(Math.random() * 100) + 1);
-    });
-
-main();
-}
-    }
-    data.forEach(item => {
-function generateRandomData() {
-
-}
-        print(f"Random Number: {item}")
-if __name__ == "__main__":
-def generate_random_data():
-    for item in data:
-
-
-    data = [random.randint(1, 100) for _ in range(10)]
-def main():
-import random
-    data = generate_random_data()
-
-
-    return data
-    main()    for item in data:
-    data = generate_random_data()
-        print(f"Random Number: {item}")
-
-import random
-    return data
     main()
 def main():
-if __name__ == "__main__":
     data = [random.randint(1, 100) for _ in range(10)]
-def generate_random_data():
 
+    return data
+
+        print(f"Random Number: {item}")
+
+    for item in data:
+if __name__ == "__main__":
+    data = generate_random_data()
+import random
+
+def generate_random_data():def generate_random_data():
+    data = [random.randint(1, 100) for _ in range(10)]
+import random
+
+if __name__ == "__main__":
+
+    main()
+def main():
+    data = generate_random_data()
+    for item in data:
+    return data
+        print(f"Random Number: {item}")
 
